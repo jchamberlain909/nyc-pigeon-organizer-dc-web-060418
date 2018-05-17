f830574141c9342d6f41676097cf57400b932f6a
jchamberlain909 <jchamberlain909@gmail.com>
2018-05-17 19:34:56 -0400

    implemented nyc_pigeon_organizer method

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,36 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+
+  new_pigeon_data = {}
+  data.each do |attribute_type, attribute_data|
+    attribute_data.each do |attribute, pigeon_list|
+      pigeon_list.each do |pigeon_name|
+        if !!new_pigeon_data[pigeon_name]
+          if !!new_pigeon_data[pigeon_name][attribute_type]
+            if attribute.instance_of? Symbol
+              new_pigeon_data[pigeon_name][attribute_type] << (attribute.id2name)
+            else
+              new_pigeon_data[pigeon_name][attribute_type] << attribute
+            end
+          else
+            if attribute.instance_of? Symbol
+              new_pigeon_data[pigeon_name][attribute_type] = [attribute.id2name]
+            else
+              new_pigeon_data[pigeon_name][attribute_type] = [attribute]
+            end
+          end
+        else
+          new_pigeon_data[pigeon_name]={}
+          if attribute.instance_of? Symbol
+            new_pigeon_data[pigeon_name][attribute_type] = [attribute.id2name]
+          else
+            new_pigeon_data[pigeon_name][attribute_type] = [attribute]
+          end
+        end
+      end
+    end
+  end
+  new_pigeon_data
 end
